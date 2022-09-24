0d393e368080db3c257577bd59ab5b35a9107e0d
nash <nash@daangn.com>
2022-09-24 23:01:01 +0900

    fix: button event duplicated

--- client/src/containers/MyEnrolment/Enrollment.js
+++ client/src/containers/MyEnrolment/Enrollment.js
@@ -3,26 +3,26 @@ import CommonPageLayout from "../../Layout/CommonPageLayout";
 import { rem } from "../../constants/style";
 import Empty from "../Common/Empty";
 
-const EnrolmentListBlock = styled.div`
+const EnrollmentListBlock = styled.div`
   margin-bottom: ${rem.lgSpacing};
   padding: ${rem.lgSpacing} ${rem.lgSpacing};
   box-shadow: 0 6px 20px -2px rgb(155 155 160 / 30%),
     0 0 1px 0 rgb(110 110 115 / 30%);
 `;
 
-function Enrolment() {
+function Enrollment() {
   return (
     <CommonPageLayout>
-      <EnrolmentListBlock>
+      <EnrollmentListBlock>
         {/* 수강신청이 존재 하지 않을 때 */}
         <Empty
           text={
             "수강신청 내역이 존재하지 않습니다. 다양한 분야의 강의들을 둘러보고 수강신청 해보세요 :)"
           }
         />
-      </EnrolmentListBlock>
+      </EnrollmentListBlock>
     </CommonPageLayout>
   );
 }
 
-export default Enrolment;
+export default Enrollment;
